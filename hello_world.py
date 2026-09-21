4470a6752f5e32901e9afeb0b4cc452977139efc
jthendry <hendrymariaj@gmail.com>
2026-09-21 17:16:03 -0400

    add my name to hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,1 @@
-print("Hello, World! Our members are: Sheharyar Khan, Myra, Jin Michael Qian,  ")
+print("Hello, World! Our members are: Sheharyar Khan, Myra, Jin Michael Qian, Hendry Jimenez Tejeda ")
